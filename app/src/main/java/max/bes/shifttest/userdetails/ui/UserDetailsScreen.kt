b85bdf5bc66ea3cfad19d8f7a7623da97430707e
Max <bespalov.m.9@ya.ru>
2024-01-28 16:04:36 +0300

    details screen design refactoring

--- app/src/main/java/max/bes/shifttest/userdetails/ui/UserDetailsScreen.kt
+++ app/src/main/java/max/bes/shifttest/userdetails/ui/UserDetailsScreen.kt
@@ -2,18 +2,23 @@ package max.bes.shifttest.userdetails.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -63,7 +68,6 @@ fun UserDetailsScreenContent(
             onAddressClick = onAddressClick
         )
     }
-
 }
 
 @Composable
@@ -94,33 +98,49 @@ fun UserInfo(
         Text(
             text = "${user.firstName} ${user.lastName}",
             fontSize = 24.sp,
+            modifier = Modifier.padding(start = 4.dp)
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
         Text(
-            text = "${user.country} ${user.city}",
+            text = "${user.country}, ${user.city}",
             fontSize = 16.sp,
             color = Blue,
             style = TextStyle(textDecoration = TextDecoration.Underline),
-            modifier = Modifier.clickable { onAddressClick(user.latitude, user.longitude) }
+            modifier = Modifier
+                .clickable { onAddressClick(user.latitude, user.longitude) }
+                .padding(start = 4.dp)
         )
 
-        TextItemInfoAsLink(user.phone, onPhoneClick)
-        TextItemInfoAsLink(user.email, onEmailClick)
+        TextItemInfoAsLink(stringResource(id = R.string.phone), user.phone, onPhoneClick)
+        TextItemInfoAsLink(stringResource(id = R.string.email), user.email, onEmailClick)
 
     }
 
 }
 
 @Composable
-fun TextItemInfoAsLink(text: String, onClick: (String) -> Unit) {
+fun TextItemInfoAsLink(title: String, text: String, onClick: (String) -> Unit) {
     Spacer(modifier = Modifier.height(4.dp))
-    Text(
-        text = text,
-        fontSize = 16.sp,
-        color = Blue,
-        style = TextStyle(textDecoration = TextDecoration.Underline),
-        modifier = Modifier.clickable { onClick(text) }
-    )
+
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 4.dp)
+    ) {
+        Text(
+            text = title,
+            fontSize = 16.sp,
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            color = Blue,
+            style = TextStyle(textDecoration = TextDecoration.Underline),
+            modifier = Modifier.clickable { onClick(text) }
+        )
+    }
+
 }
